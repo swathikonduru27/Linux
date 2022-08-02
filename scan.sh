609a9775be513d28d85b3e1ca13b4f7528099457
swathikonduru27 <78338618+swathikonduru27@users.noreply.github.com>
2022-08-02 18:58:29 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,1 +1,15 @@
-
+#!/bin/bash
+sudo apt-get install geoip-bin > /dev/null
+array=()
+grep "Invalid user" /var/log/auth.log >> holding.log
+readarray array < holding.log
+ip=""
+country=""
+date=""
+for i in "${array[@]}"; do
+        ip="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$i")"
+        country="$(geoiplookup -s $ip)"
+        date=$(echo "$i" | cut -c1-15)
+        echo "$ip ""$country ""$date" >> /var/webserver_log/unauthorized.log
+done
+sudo rm holding.log
