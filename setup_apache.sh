2c580ba04d1e66c94a248da70e43e78fe5ab6b28
swathikonduru27 <78338618+swathikonduru27@users.noreply.github.com>
2022-08-02 18:57:40 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -1,6 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
 set -x
 
 sudo apt update
 sudo apt install -y apache2
+sudo apt install -y nfs-kernel-server
+sudo mkdir /var/webserver_log
+sudo chown nobody:nogroup /var/webserver_log
+echo "/var/webserver_log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+sudo systemctl restart nfs-kernel-server
