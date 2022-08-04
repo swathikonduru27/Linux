bc45d80ec1f4f6284be9e6626ade12acbab4e7ad
swathikonduru27 <78338618+swathikonduru27@users.noreply.github.com>
2022-08-04 11:25:53 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
-sudo apt-get install geoip-bin > /dev/null
-array=()
-grep "Invalid user" /var/log/auth.log >> holding.log
-readarray array < holding.log
-ip=""
-country=""
-date=""
-for i in "${array[@]}"; do
-        ip="$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' <<< "$i")"
-        country="$(geoiplookup -s $ip)"
-        date=$(echo "$i" | cut -c1-15)
-        echo "$ip ""$country ""$date" >> /var/webserver_log/unauthorized.log
+ip_address=($(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $10}'))
+month=($(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $1}'))
+day=($(sudo cat /var/log/auth.log | grep "Invalid user" | awk '{print $2}')) 
+
+ips_list=()
+month_list=()
+day_list=()
+
+for ip in $ip_address; do
+        ips_list+=${ip}
 done
-sudo rm holding.log
+
+for date in ${month}; do
+        month_list+=${date}
+done
+
+for date in ${day}; do
+        day_list+=${date}
+done
+
+#echo "${day_list[*]}"
+countries={}
+for ip in $ips; do
+        country=$(curl ip_addressinfo.io/$ip/country)
+        countries+=${country}
+        echo ${country}
+done
+
+for ip in ${!countries[*]}; do
+        echo "${ips_list[$ip]} ${countries[$ip]} ${month_list[$ip]} ${day_list[$ip]}" > /var/webserver_log/unauthorized.log
+done
