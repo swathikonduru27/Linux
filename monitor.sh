9120942f0790ec38bebd562cc6cca2423ef5626c
swathikonduru27 <78338618+swathikonduru27@users.noreply.github.com>
2022-08-04 11:42:56 -0400

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+old_c=$(head -1 /var/webserver_monitor/counter_file.txt)
+echo $old_c
+new_c=$(wc -l < /var/webserver_monitor/unauthorized.log)
+new_l=`expr $new_c - $old_c`
+if ((new_l > 0))
+then
+        tail -n $new_l  /var/webserver_monitor/unauthorized.log | mail -s "New entries" m7684123@gmail.com
+else
+        echo "No unauthorized access" | mail -s "New entries" m7684123@gmail.com
+fi
+echo $new_l > /var/webserver_monitor/counter.txt
