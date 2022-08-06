6a7685bb060fcd1598b6d0a31f72845fd8f313bf
swathikonduru27 <78338618+swathikonduru27@users.noreply.github.com>
2022-08-05 23:04:03 -0400

    Update setup_apache.sh

--- setup_apache.sh
+++ setup_apache.sh
@@ -9,3 +9,4 @@ sudo mkdir /var/webserver_log
 sudo chown nobody:nogroup /var/webserver_log
 echo "/var/webserver_log 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 sudo systemctl restart nfs-kernel-server
+sudo chmod -x scan.sh
